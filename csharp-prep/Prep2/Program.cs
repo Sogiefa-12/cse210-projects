c46956fa2ea3ffd022c396c20c0ce1781b7bca9e
Sogiefa-12 <ogi23001@byui.edu>
2024-01-10 16:39:00 +0100

    commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.Write("what is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter = "";
+        if (grade >=90)
+        {
+            letter = "A";
+            
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+
+        }
+        else if (grade >=70)
+        {
+            letter = "B";
+        }
+        else if (grade >= 60)
+        {
+            letter = "C";
+        }
+        else 
+        {
+            letter = "D";
+        }
+
+        Console.WriteLine($"Your Grade is: {letter}");
+
+
+        if (grade >= 70)
+        {
+            Console.Write("Congratulations!! You did well");
+        }
+        else
+        {
+            Console.Write("try again, put more effort next time");
+        }
+
+       
+
     }
 }
