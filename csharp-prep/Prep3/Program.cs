28381f2884af8f4a6225e4a83165494542b96acb
Sogiefa-12 <ogi23001@byui.edu>
2024-01-11 14:27:39 +0100

    commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+      
+       Console.Write("What is the magic Number");
+       string response = Console.ReadLine();
+       int magicNumber = int.Parse(response);
+       Console.Write("what is your Guess?");
+       string guess = Console.ReadLine();
+       int guessNumber = int.Parse(guess);
+
+       Random randomGenerator = new Random();
+       int magicNumber = randomGenerator.Next(1, 101);
+
+
+        guessNumber =-1;
+
+        while (guessNumber = magicNumber)
+        {
+            
+       
+      
+            if (guessNumber > magicNumber)
+            { 
+                Console.Write("Higher");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.Write("Lower");
+            }
+
+            else
+            {
+                Console.Write("You guessedit!!");
+            }
+        
+         }
+
+
+
+       }
+
+    
 }
