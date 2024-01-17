6eaeb1dd94b0259e8300b3c6ffdf0d7f083b58c8
Sogiefa-12 <ogi23001@byui.edu>
2024-01-17 14:02:07 +0100

    commit

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,20 @@
+using System;
+
+//#pragma warning disable CA1050 // Declare types in namespaces
+public class Job
+//#pragma warning restore CA1050 // Declare types in namespaces
+ {
+    public string _company;
+
+    public string _jobTitle;
+
+    public int _startYear;
+
+    public int _endYear;
+
+    public void Display() 
+    {
+        Console.WriteLine($"{_jobTitle} {_company}) {_startYear}-{_endYear}");
+    }
+
+}
