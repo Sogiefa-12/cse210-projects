e05abf376f1f602e229e5e2bf724f6b98031dfa1
Sogiefa-12 <ogi23001@byui.edu>
2024-01-24 14:42:24 +0100

    commit

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,39 @@
+using System;
+
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _numerator = wholeNumber;
+        _numerator =1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _numerator = top;
+        _denominator = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_numerator}/{_denominator}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / (double)_denominator;
+    }
+
+
+}
